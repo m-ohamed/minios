a52776e158d5c9ff83faaee31f8885882c8204ff
CptDynamiX <mohamed.adel-ibrahim@student.guc.edu.eg>
2017-04-07 15:34:08 +0200

    added copy and dir commands

--- shell.c
+++ shell.c
@@ -6,14 +6,19 @@ void exec(char*, int);
 
 int comparestr(char*, char*);
 
+int rem(int, int);
+
+int div(int, int);
+
 char buffer[13312];
 
 int main()
 {
-  char input[20];
+  char input[30];
 
   while(1)
   {
+    interrupt(0x21, 0, "\r", 0, 0);
     interrupt(0x21, 0, "SHELL>\0", 0, 0);
     interrupt(0x21, 1, input, 0, 0);
 
@@ -35,10 +40,34 @@ int main()
         }
         else
         {
-          interrupt(0x21, 0, "Bad command!\n\b\b\b\b\b\b\b\b\b\b\b\b\0", 0, 0);
+          if(comparestr(split_string(input), "copy\0") == 1)
+          {
+            exec(input, 3);
+          }
+          else
+          {
+            if(comparestr(split_string(input), "dir\0") == 1)
+            {
+              exec(input, 4);
+            }
+            else
+            {
+              if(comparestr(split_string(input), "create\0") == 1)
+              {
+                exec(input, 5);
+              }
+              else
+              {
+                interrupt(0x21, 0, "Bad command!\n\r\0", 0, 0);
+              }
+            }
+          }
         }
       }
     }
+
+    interrupt(0x21, 0, "\n\r\0", 0, 0);
+
   }
 }
 
@@ -46,9 +75,16 @@ void exec(char* input, int op)
 {
   int c;
   int i;
+  int j;
   int counter;
   char secoutput[6];
+  char thirdfile[7];
+  char directory[512];
+  char line[100];
+
   counter = 0;
+  i = 0;
+  j = 0;
 
   for(c = 0; c < 13312; c++)
   {
@@ -58,7 +94,7 @@ void exec(char* input, int op)
   switch(op)
   {
     case 0:
-      for(i = 5; counter < 6; i++)
+      for(i = 5; counter < 6 && input[i] != '\0'; i++)
       {
         secoutput[i-5] = input[i];
         counter++;
@@ -77,13 +113,13 @@ void exec(char* input, int op)
       }
       else
       {
-        interrupt(0x21, 0, "File not found!\n\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\0", 0, 0);
+        interrupt(0x21, 0, "File not found!\n\r\0", 0, 0);
       }
 
       break;
 
     case 1:
-      for(i = 8; counter < 6; i++)
+      for(i = 8; counter < 6 && input[i] != '\0'; i++)
       {
         secoutput[i-8] = input[i];
         counter++;
@@ -99,7 +135,7 @@ void exec(char* input, int op)
       break;
 
     case 2:
-      for(i = 7; counter < 6; i++)
+      for(i = 7; counter < 6 && input[i] != '\0'; i++)
       {
         secoutput[i-7] = input[i];
         counter++;
@@ -112,6 +148,166 @@ void exec(char* input, int op)
 
       interrupt(0x21, 7, secoutput, 0, 0);
 
+      break;
+
+    case 3:
+      for(i = 5; input[i] != ' '; i++)
+      {
+        if(counter < 6)
+        {
+          secoutput[i-5] = input[i];
+          counter++;
+        }
+      }
+
+      i = i + 1;
+      j = 0;
+
+      for(i; input[i] != '\0'; i++)
+      {
+        if(j < 6)
+        {
+          thirdfile[j] = input[i];
+          j++;
+        }
+      }
+
+
+      for(counter; counter < 6; counter++)
+      {
+        secoutput[counter] = 0x00;
+      }
+
+      for(j; j < 6; j++)
+      {
+        thirdfile[j] = 0x00;
+      }
+
+      interrupt(0x21, 3, secoutput, buffer, 0);
+
+      i = 0;
+      j = 0;
+
+      for(i; i < 26; i++)
+      {
+        for(j = 0; j < 512; j++)
+        {
+          if(buffer[(i*512) + j] != 0x00)
+            break;
+        }
+      }
+
+      if(buffer[0] != 0x00)
+      {
+        interrupt(0x21, 8, thirdfile, buffer, i);
+        interrupt(0x21, 0, "File copied successfully!\n\r\0", 0, 0);
+      }
+      else
+      {
+        interrupt(0x21, 0, "File not found!\n\r\0", 0, 0);
+      }
+
+      break;
+
+    case 4:
+
+      interrupt(0x21, 2, directory, 2, 0);
+
+      i = 0;
+      j = 0;
+
+      for(i = 0; i < 512; i = i + 32)
+      {
+        while(directory[i] == 0x00 && i < 512)
+          i = i + 32;
+          
+
+        for(j = 0; j < 6; j++)
+          thirdfile[j] = 0x00;
+
+        for(j = 0; j < 6; j++)
+        {
+          if(directory[i+j] == 0x00)
+          {
+            break;
+          }
+
+          thirdfile[j] = directory[i+j];
+        }
+
+        thirdfile[6] = 0x00;
+
+        interrupt(0x21, 0, "name: ", 0, 0);
+        interrupt(0x21, 0, thirdfile, 0, 0);
+        interrupt(0x21, 0, "\n\r\0", 0, 0);
+        interrupt(0x21, 0, "size: ", 0, 0);
+
+        counter = 0;
+
+        for(j = 6; j < 32; j++)
+        {
+          if(directory[i+j] == 0x00)
+            break;
+
+          counter++;
+        }
+
+        line[0] = div(counter, 10) + 48;
+        line[1] = rem(counter, 10) + 48;
+        line[2] = '\0';
+
+        interrupt(0x21, 0, line, 0, 0);
+        interrupt(0x21, 0, "\n\n\r\0", 0, 0);
+
+      }
+
+      break;
+
+    case 5:
+      for(i = 7; counter < 6 && input[i] != '\0'; i++)
+      {
+        secoutput[i-7] = input[i];
+        counter++;
+      }
+
+      for(counter; counter < 6; counter++)
+      {
+        secoutput[counter] = 0x00;
+      }
+
+      i = 0;
+
+      while(1)
+      {
+        for(j = 0; j < 100; j++)
+          line[j] = 0x00;
+
+        interrupt(0x21, 0, ":>\0", 0, 0);
+
+        interrupt(0x21, 1, line, 0, 0);
+
+
+        if(line[0] == 0xA)
+          break;
+
+        for(j = 0; line[j] != 0x00; j++)
+          buffer[j+i] = line[j];
+
+        i = i + j;
+
+      }
+
+      j = 0;
+
+      j = div(i,512);
+
+      if(rem(i,512) != 0)
+        j += 1;
+
+      interrupt(0x21, 8, secoutput, buffer, j);
+      interrupt(0x21, 0, "File write successful!\n\r\0", 0, 0);
+
+      break;
 
     default: break;
   }
@@ -132,6 +328,58 @@ char* split_string(char* input)
 
     firstoutput[i] = input[i];
   }
+
+  return firstoutput;
+}
+
+int div(int nu, int de)
+{
+	int temp = 1;
+	int quotient = 0;
+	while (de <= nu)
+	{
+		de <<= 1;
+		temp <<= 1;
+	}
+	while (temp > 1)
+	{
+		de >>= 1;
+		temp >>= 1;
+		if (nu >= de)
+		{
+			nu -= de;
+			quotient += temp;
+		}
+	}
+	return quotient;
+}
+
+int rem(int num, int divisor)
+{
+	int i = 1;
+    int product = 0;
+
+    if (divisor == 0)
+    {
+        return -1;
+    }
+
+    if (divisor < 0)
+    	divisor = -divisor;
+
+
+    if (num < 0)
+    	num = -num;
+
+
+
+    while (product <= num)
+    {
+        product = divisor * i;
+        i++;
+    }
+
+    return num - (product - divisor);
 }
 
 int comparestr(char* first, char* sec)
